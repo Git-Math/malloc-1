96d8a70300cac7d1bd77b111c2f4edc2a14fc4a3
JeanMax <mcanal@student.42.fr>
2018-04-27 17:41:11 +0200

    Fix realloc

--- src/realloc.c
+++ src/realloc.c
@@ -6,7 +6,7 @@
 /*   By: mc <mc.maxcanal@gmail.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 21:07:40 by mc                #+#    #+#             */
-/*   Updated: 2018/04/25 07:11:37 by mc               ###   ########.fr       */
+/*   Updated: 2018/04/27 17:34:17 by mcanal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ void			*realloc(void *ptr, size_t size)
 	void	*new;
 	t_block	*block;
 
-	if (!ptr || (size_t)ptr % PADDING)
-		return (NULL);
-	pthread_mutex_lock(&g_mutex);
-	block = find_block_by_addr(ptr);
-	pthread_mutex_unlock(&g_mutex);
-	if (!block)
-		return (NULL);
+	if (ptr && !((size_t)ptr % PADDING))
+	{
+		pthread_mutex_lock(&g_mutex);
+		block = find_block_by_addr(ptr);
+		pthread_mutex_unlock(&g_mutex);
+	}
+	else
+		block = NULL;
 	new = malloc(size);
-	if (new && ptr)
+	if (new && ptr && block)
 	{
 		ft_memcpy(new, ptr, (block->size > size) ? size : block->size);
 		free(ptr);
